d36006a6fa0fda0d58596633c040c725a1b236dc
Vikas Kumar Gautam <vikacse@gmail.com>
2025-06-30 23:16:56 +0530

    find all indexes of an element in list

---
+++ Recursion/all_index.py
@@ -0,0 +1,10 @@
+def find_all_index(array, i, x):
+    if i==len(array):
+        return []
+    arr = find_all_index(array, i + 1, x)
+    if x==array[i]:
+        arr.append(i)
+    return arr
+
+list = [1,2,3,4,2,5,2,6,5,4,2]
+print(find_all_index(list,0,2))
