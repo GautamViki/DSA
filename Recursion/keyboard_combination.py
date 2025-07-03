3fae8d2968a77944d3e665a54ad4fa8a25812344
Vikas Kumar Gautam <vikacse@gmail.com>
2025-07-03 23:17:15 +0530

    get all keyboard combinations

--- Recursion/keyboard_combination.py
+++ Recursion/keyboard_combination.py
@@ -1,39 +1,15 @@
-def get_keyboard_combinations(str1,str2,i,res):
-    if i==len(str1):
-        return res
-    char1=str1[i]
-    for char2 in str2:
-        res.append(char1+char2)
-    return get_keyboard_combinations(str1,str2,i+1,res)
+def get_keyboard_combinations(keys,key_list):
+    if len(keys)==0:
+        return [""]
+    num = keys[0]
+    subkeys = keys[1:]
+    constructed_str = get_keyboard_combinations(subkeys,key_list)
+    res = []
+    for char in key_list[int(num)]:
+        for str in constructed_str:
+            res.append(char+str)
+    return res
 
-keyboard_dict={
-    1:"abc",
-    2:"def",
-    3:"ghi",
-    4:"jkl",
-    5:"mno",
-    6:"pqr",
-    7:"stu",
-    8:"vwx",
-    9:"yz",
-    0:"?!"
-}
-pressed_key=12
-arr = []
-while pressed_key>0:
-    n=pressed_key%10
-    arr.append(n)
-    pressed_key = pressed_key/10
-print(arr)
-i=0
-j=len(arr)-1
-while i<j:
-    temp=arr[i]
-    arr[i]=arr[j]
-    arr[j]=temp
-    i=i+1
-    j=j-1
-
-print("-----",arr)
-# res=get_keyboard_combinations(keyboard_dict.get(arr[1]),keyboard_dict.get(arr[2]),0,[])
-# print(res)
+key_list = ['?!','abc','def','ghi','ijk','lmn','opq','rst','uvw','xyz']
+words = get_keyboard_combinations('03',key_list)
+print(words)
