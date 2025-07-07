4024d050290abaa4d85f81f49eb93c626eff8ff3
Vikas Kumar Gautam <vikacse@gmail.com>
2025-07-07 23:26:02 +0530

    print stair paths

--- Recursion/stair_path.py
+++ Recursion/stair_path.py
@@ -16,3 +16,15 @@ def get_stair_path(n):
     return paths
 
 print(get_stair_path(4))
+
+def print_stair_path(n,ans):
+    if n==0:
+        print(ans)
+        return
+    elif n<0:
+        return
+    print_stair_path(n-1,ans+'1')
+    print_stair_path(n-2,ans+'2')
+    print_stair_path(n-3,ans+'3')
+
+print_stair_path(4,'')
