ea4e72b4f0a4831201b97e43ebaec532c325db82
Vikas Kumar Gautam <vikacse@gmail.com>
2025-07-09 23:33:31 +0530

    print all permutations

--- Recursion/permutation.py
+++ Recursion/permutation.py
@@ -2,7 +2,6 @@ def get_permutations(word,ans):
     if len(word)==0:
         print(ans)
         return
-
     for i in range(len(word)):
         get_permutations(word[:i]+word[i+1:],ans+word[i])
 
