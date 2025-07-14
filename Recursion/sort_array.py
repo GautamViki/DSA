696052f9e6db355f5e110c261d1d18a2ff4f5a08
Vikas Kumar Gautam <vikacse@gmail.com>
2025-07-15 01:08:03 +0530

    sort list

--- Recursion/sort_array.py
+++ Recursion/sort_array.py
@@ -7,13 +7,13 @@ def insert_in_sorted_list(arr,ele):
     sorted_arr.append(last_ele)
     return sorted_arr
 
-def sort_list(arr,n):
-    if n==0:
+def sort_list(arr):
+    if len(arr)==1:
         return arr
     last_ele = arr.pop()
-    new_arr= sort_list(arr,n-1)
+    new_arr= sort_list(arr)
     sorted_arr= insert_in_sorted_list(new_arr,last_ele)
     return sorted_arr
 
-arr= [3,2,1,0]
-print(sort_list(arr,len(arr)-1))
+arr= [3,2,1,0,5,-1]
+print(sort_list(arr))
