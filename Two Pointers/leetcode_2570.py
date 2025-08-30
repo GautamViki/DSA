ff787c3d7d4df3abad6d96208ad2b75a2e4e787c
Vikas Kumar Gautam <gautamvikas@Vikass-MacBook-Air.local>
2025-08-31 00:07:27 +0530

    leetcode 2570

---
+++ Two Pointers/leetcode_2570.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def twoPointers(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        n = len(nums1)
+        m = len(nums2)
+        i = j = 0
+        result= []
+        while i<n and j<m:
+            if nums1[i][0] < nums2[j][0]:
+                result.append(nums1[i])
+                i=i+1
+            elif nums1[i][0] > nums2[j][0]:
+                result.append(nums2[j])
+                j=j+1
+            else:
+                nums1[i][1]=nums1[i][1]+nums2[j][1]
+                result.append(nums1[i])
+                i=i+1
+                j=j+1
+
+        while i<n:
+            result.append(nums1[i])
+            i=i+1
+        while j<m:
+            result.append(nums2[j])
+            j=j+1
+        return result
+
+
+nums1 = [[1,2],[2,3],[3,4],[5,6]]
+nums2 = [[1,2],[3,4],[4,2],[5,6]]
+solution = Solution()
+res = solution.twoPointers(nums1, nums2)
+print(res)
